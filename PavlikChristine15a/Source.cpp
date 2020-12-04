a7892ffe7c780fec012cc2ef130eeb67eb191045
Christine Pavlik <christine.pavlik@sinclair.edu>
2020-12-04 11:32:51 -0500

    This is my final version. This includes two solutions to the problem.

--- PavlikChristine15a/Source.cpp
+++ PavlikChristine15a/Source.cpp
@@ -8,6 +8,46 @@ CIS 1202 501
 #include <string>
 using namespace std;
 ///////////////////////////////////////////
+//I was not sure which way to do this so I did it both ways.
+//At first, the nonclass version was not working becuase I was throwing the string like this (throw "invalidRangeException";)
+char character(char start, int offset) {
+	if (start >= 'a' && start <= 'z') {
+		int both = static_cast<int>(start) + offset;
+		if (both >= 97 && both <= 122) {
+			char it = static_cast<char>(both);
+			return it;
+		}
+		else {
+			string range = "invalidRangeException";
+			throw range;
+		}
+	}
+	else if (start >= 'A' && start <= 'Z') {
+		int both = static_cast<int>(start) + offset;
+		if (both >= 65 && both <= 90) {
+			char it = static_cast<char>(both);
+			return it;
+		}
+		else {
+			string range = "invalidRangeException";
+			throw range;
+		}
+	}
+	else {
+		string charExcept = "invalidCharacterException";
+		throw charExcept;
+	}
+}
+int main() {
+	try {
+		cout << character('R', -2);
+	}
+	catch (string name) {
+		cout << "You have run into a " << name;
+	}
+	return 0;
+}
+/*
 class invalidCharacterException {
 
 };
@@ -51,3 +91,4 @@ int main() {
 	}
 	return 0;
 }
+*/
